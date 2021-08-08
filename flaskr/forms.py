4bd0d05362ea8ed9715e2fb592c3417149a18940
willwray14 <17087@burnside.school.nz>
2021-08-09 09:41:37 +1200

    finished off dashboard widgets

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, TextField, DecimalField, DateField, SubmitField
 from wtforms.validators import DataRequired
-from flask_login import current_user
 
 class AddHours(FlaskForm):
      description = TextField('Description', validators=[DataRequired()])
