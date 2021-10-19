4be2186edf6661c685aeebf9c5e3c99df529b546
willwray14 <17087@burnside.school.nz>
2021-10-19 20:32:11 +1300

    finished edit/delete functionality for past logs

--- flaskr/run (1).ps1
+++ flaskr/run (1).ps1
@@ -2,4 +2,4 @@ $env:FLASK_APP="flaskr"
 $env:FLASK_DEBUG=1
 $env:GOOGLE_CLIENT_ID='201549234557-q6qrci2pp2m6mt077crps8s68co2s6gl.apps.googleusercontent.com'
 $env:GOOGLE_CLIENT_SECRET='RQjP5bin1qvQgHrVKCahcD1D'
-flask run --cert=adhoc
+flask run --cert='cert.pem' --key='key.pem'
